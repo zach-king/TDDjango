acd2ecdfa28138f24c724d89454682ed1be0fb3b
Zachary King <kingzach77@gmail.com>
2016-07-06 21:53:03 -0500

    Model for list Items and associated migration

--- tdd-workshop/lists/tests.py
+++ tdd-workshop/lists/tests.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
 
 from lists.views import home_page
+from lists.models import Item
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -36,3 +37,21 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
